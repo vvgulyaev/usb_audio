6945ffc5592750f0b9e5523d46cf6fc1fdeec920
Vyacheslav Gulyaev <vv_gulyaev@mail.ru>
2020-10-26 16:21:31 +0300

    remove *.d files from repo

--- Projects/STM32F769I-Discovery/Applications/USB_Device/AUD_Streaming10/SW4STM32/STM32F769I-DISCO_UAC10-REC/Debug/Application/Extension/Drivers/BSP/Components/wm8994/wm8994_ex.d
+++
@@ -1,8 +0,0 @@
-Application/Extension/Drivers/BSP/Components/wm8994/wm8994_ex.o: \
- /home/homepc/работа/usb_audio/proj/en.x-cube-usb-audio/STM32CubeExpansion_USBAudioStreaming_V1.0.0/Projects/STM32F769I-Discovery/Applications/USB_Device/Extension/Drivers/BSP/Components/wm8994/wm8994_ex.c \
- /home/homepc/работа/usb_audio/proj/en.x-cube-usb-audio/STM32CubeExpansion_USBAudioStreaming_V1.0.0/Projects/STM32F769I-Discovery/Applications/USB_Device/Extension/Drivers/BSP/Components/wm8994/wm8994_ex.h \
- ../../../../../../../../Drivers/BSP/Components/common/audio.h
-
-/home/homepc/работа/usb_audio/proj/en.x-cube-usb-audio/STM32CubeExpansion_USBAudioStreaming_V1.0.0/Projects/STM32F769I-Discovery/Applications/USB_Device/Extension/Drivers/BSP/Components/wm8994/wm8994_ex.h:
-
-../../../../../../../../Drivers/BSP/Components/common/audio.h:
--- Projects/STM32F769I-Discovery/Applications/USB_Device/AUD_Streaming10/SW4STM32/STM32F769I-DISCO_UAC10-REC/Debug/Application/SW4STM32/startup_stm32f769xx.d
+++
@@ -1,2 +0,0 @@
-Application/SW4STM32/startup_stm32f769xx.o: \
- /home/homepc/работа/usb_audio/proj/en.x-cube-usb-audio/STM32CubeExpansion_USBAudioStreaming_V1.0.0/Projects/STM32F769I-Discovery/Applications/USB_Device/AUD_Streaming10/SW4STM32/startup_stm32f769xx.s
